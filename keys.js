902789f15454d424544982acbdda7fb5035ab890
Ruslan Tugov <ruslan.tugov@gmail.com>
2019-09-09 11:26:09 -0400

    1

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+exports.omdb = {
+  api_key: process.env.OMDB_API_KEY
+};
